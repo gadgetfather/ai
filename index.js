ff7271189ddd10c4ec979422a19eebcfceee8f5a
Akshay <akshay.kamble@copods.co>
2024-02-28 19:49:50 +0530

    hello world

---
+++ index.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import OpenAI from "openai";
+const openai = new OpenAI({});
+const results = await openai.chat.completions.create({
+  model: "gpt-3.5-turbo",
+  messages: [
+    {
+      role: "system",
+      content: "You are a helpful assistant.",
+    },
+    {
+      role: "user",
+      content: "hi, can you tell me a joke?",
+    },
+  ],
+});
+
+console.log(results.choices);
