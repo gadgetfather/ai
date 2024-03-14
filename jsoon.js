b1c562ddc5702ce57ea3b42e810c44599e1b7da0
DhruvCopods <dhruv.samant@copods.co>
2024-03-14 21:15:27 +0530

    update: jsoon for machine name

--- jsoon.js
+++ jsoon.js
@@ -15,23 +15,23 @@ export async function jsoon(content) {
         content: `
         Convert the following content into JSON format. JSON format should be in the following structure: 
         {
+          machineName: "Name of the machine on which the content is based",
           modules:[
           {
-          id:"",
+          id:"module index",
           moduleName: "Name of the module, dont add Training module 1 or 2 at start",
           estimatedTime: "time in minutes to complete the module",
           totalTopics: "total number of topics in the module",
           shortModuleDescription: "short description of the module",
           ModuleContent: [
             {
-              id:"unique id",
+              id:"ModuleContent index",
               title: "Title of the topic",
               titleDescription: "One liner description of the topic",
               image: "image url related to the topic",
               content: "This is the content of the module broken down into smaller parts"
             }
           ]
-
         }
       }
     ]
@@ -41,6 +41,23 @@ export async function jsoon(content) {
     model: "gpt-3.5-turbo-1106",
     response_format: { type: "json_object" },
   });
+
+  //  assessment: {
+  //    questions: [
+  //      {
+  //        id: "questions index",
+  //        question: "Question",
+  //        options: [
+  //          { id: "unique id", option: "Option 1" },
+  //          { id: "unique id", option: "Option 2" },
+  //          { id: "unique id", option: "Option 3" },
+  //          { id: "unique id", option: "Option 4" },
+  //        ],
+  //        answer: "Correct option id",
+  //      },
+  //    ];
+  //  }
+
   console.log(completion.choices[0].message.content);
   const docRef = await addDoc(collection(db, "modules"), {
     createdAt: Date.now(),
